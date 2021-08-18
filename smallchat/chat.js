74d09be053f86c7b8c225014017c59928a8c4091
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-08-18 13:56:54 +0300

    basic namespaces and rooms

--- smallchat/chat.js
+++ smallchat/chat.js
@@ -9,18 +9,38 @@ app.use(express.static(path.join(__dirname, "/public")));
 const expressServer = app.listen(8000, () => {
   console.log("server listen on port 8000");
 });
+
 const io = new Server(expressServer, {
   cors: {
     origin: "*",
   },
 });
+
 //'connect' is the same as 'connection'
-//create socket io server (io)
+//create socket io server (io) / namespace
 io.on("connection", (socket) => {
+  console.log("Connect to main namespace");
+
   //receive event
   //when receive message from client emit message to all clients
   socket.on("userMessageToServer", (message) => {
     //need to use Server(io) NOT single socket
     io.emit("userMessageToClient", message);
   });
+  //the Server can communicate across namespace but the Client need to be in THAT namespace in order to get the event
+  //ROOMS
+  //join room level1
+  socket.join("level1");
+  //send message to the room, comes to everybody exept who send it
+  socket.to("level1").emit("joined", `${socket.id} joins to level 1 room`);
+  //send message to the namespace, comes to everybody
+  io.of("/")
+    .to("level1")
+    .emit("joined", `${socket.id} joins to level 1 room from namespace`);
+});
+
+//create admin namespace
+io.of("/admin").on("connection", () => {
+  console.log("Connect to admin namespace");
+  io.of("/admin").emit("welcome", { data: "Welcome to admin chanel" });
 });
--- smallchat/public/scripts.js
+++ smallchat/public/scripts.js
@@ -1,16 +1,29 @@
 //get socket.io
 const socket = io("http://localhost:8000");
+const adminSocket = io("http://localhost:8000/admin");
+// socket.on("connect", () => {
+//   console.log("Client connect to main namespace");
+
+//   console.log(socket.id);
+// });
+adminSocket.on("welcome", (dataFromServer) => {
+  console.log("Client connect to admin namespace");
+  console.log(adminSocket.id);
+  console.log(dataFromServer);
+});
+//joining room from the main namespace
+socket.on("joined", (data) => {
+  console.log(data);
+});
 //get value from text area
-document
-  .querySelector("#message-form")
-  .addEventListener("submit", (event) => {
-    event.preventDefault();
-    const message = document.querySelector("#user-message").value;
-    //send value to the server
-    socket.emit("userMessageToServer", {
-      data: message,
-    });
+document.querySelector("#message-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const message = document.querySelector("#user-message").value;
+  //send value to the server
+  socket.emit("userMessageToServer", {
+    data: message,
   });
+});
 //receive value from the server
 socket.on("userMessageToClient", (message) => {
   const listOfMessages = document.querySelector("#messages");
